74a4eedb4f7e016c055f190ee5b60a8fc9d28e00
segatcm <segatheycallme@proton.me>
2025-01-21 11:01:38 +0100

    tasklist misc

--- frontend/src/pages/TaskList.tsx
+++ frontend/src/pages/TaskList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Paper from "../components/Paper"
 
 interface Task {
@@ -8,34 +9,73 @@ interface Task {
 const tasks: Task[] | null =
   [
     { message: "do the laundry", status: "To-Do" },
-    { message: "do the bed", status: "To-Do" },
+    { message: "do the bed", status: "Done" },
     { message: "do the clock", status: "To-Do" },
-    { message: "do the radiator", status: "To-Do" },
+    { message: "do the radiator", status: "In Progress" },
   ]
 
+const statuses = ["To-Do", "In Progress", "Done"];
+const color_lookup = new Map()
+color_lookup.set("To-Do", "text-slate-700");
+color_lookup.set("In Progress", "text-sky-700");
+color_lookup.set("Done", "text-green-700");
+
+function cycleStatus(currStatus: string, direction: number) {
+  let idx = statuses.findIndex((el) => el === currStatus) + direction;
+  if (idx < 0) {
+    idx = 2
+  }
+  if (idx > 2) {
+    idx = 0
+  }
+  return statuses[idx]
+}
+
 function TaskList() {
+  const [editID, setEditID] = useState(-1);
+  const [editValue, setEditValue] = useState<string | null>("");
+
   return (
     <div className="flex items-center justify-center h-full">
       <Paper className="h-3/4 w-full sm:w-3/4">
         {tasks ?
-          <table className="w-full">
-            <tr>
-              <th>Task message</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-            {tasks.map((task) => {
-              return (
-                <tr className="">
-                  <td className="w-4/5">{task.message}</td>
-                  <td className="w-1/6 text-center">
-                    <button className="font-bold">{"<"}</button>
-                    <span> {task.status} </span>
-                    <button className="font-bold">{">"}</button></td>
-                  <td className="text-center">e</td>
-                </tr>
-              )
-            })}
+          <table className="w-full border-collapse">
+            <thead>
+              <tr>
+                <th className="border-2 w-4/5">Task message</th>
+                <th className="border-2 w-1/6">Status</th>
+                <th className="border-2 px-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task, i) => {
+                return (
+                  <tr className="text-lg">
+                    <td className="border-2">{editID === i ?
+                      <div className="flex">
+                        <input type="text" className="bg-slate-200 border border-slate-400 pl-1 w-full" value={editValue || ""} onChange={(e) => setEditValue(e.target.textContent)} />
+                        <button className="ml-8 mr-2" onClick={() => {
+                          setEditID(-1);
+                        }}>ok</button>
+                      </div>
+                      : <span className="pl-1">{task.message}</span>}</td>
+                    <td className="text-center border-2">
+                      <button className="font-bold" onClick={() => console.log(cycleStatus(task.status, -1))}>{"<"}</button>
+                      <span className={color_lookup.get(task.status)}> {task.status} </span>
+                      <button className="font-bold" onClick={() => console.log(cycleStatus(task.status, +1))}>{">"}</button>
+                    </td>
+                    <td className="text-center border-2">
+                      <button className="mr-2" onClick={() => {
+                        setEditID(i);
+                        setEditValue(task.message);
+                      }}>e</button>
+                      <button onClick={() => {
+                      }}>d</button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
           </table>
           : "no tasks"}
       </Paper>
