b5a4a7c632b7fe3917e037641008cb9f63e3bf53
mehmedmekic08 <mekicmehmed2008@gmail.com>
2025-02-04 18:35:55 +0100

    navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="fixed border-2 border-sky-500 pb-8 w-full text-2xl font-bold text-sky-700  ">
+      TO-DO
+      <button className="flex items-center justify-center">dugme</button>
+    </div>
   )
 }
 
