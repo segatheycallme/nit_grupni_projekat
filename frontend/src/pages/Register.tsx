58f770930cc4c776e6aac4ea9f0eaa05422d8609
segatcm <segatheycallme@proton.me>
2025-02-04 18:16:30 +0100

    authentication errors and primitive validation

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -13,37 +13,45 @@ function Register() {
   return (
     <div className="text-xl flex items-center justify-center h-full">
       <Paper className="flex flex-col items-center h-min-2/5 w-full sm:w-2/3 md:w-1/2 lg:w-1/3">
-        <div className="flex flex-col space-y-4 w-full mb-auto">
-          <h1 className="text-center text-3xl mt-6">Register</h1>
-          <input
-            type="text"
-            placeholder="Enter your username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500" />
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500" />
-          <input
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500" />
-        </div>
-        <div className="flex flex-col items-center">
-          <button className="rounded-lg bg-sky-600 hover:bg-sky-500 px-4 py-1 mt-4 text-white" onClick={() => {
-            register(username, email, password);
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          register(username, email, password).then(() => {
             navigate("/");
-          }}>Register</button>
-          <NavLink to="/"> <button className="text-gray-400 text-md m-2 hover:text-gray-500">Already have an account? Click here</button></NavLink>
-        </div>
+          }).finally(() => {
+            setUsername("");
+            setEmail("");
+            setPassword("");
+          });
+        }}>
+          <div className="flex flex-col space-y-4 w-full mb-auto">
+            <h1 className="text-center text-3xl mt-6">Register</h1>
+            <input
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500" />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500" />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-sky-500" />
+          </div>
+          <div className="flex flex-col items-center">
+            <button className="rounded-lg bg-sky-600 hover:bg-sky-500 px-4 py-1 mt-4 text-white" type="submit">Register</button>
+            <NavLink to="/"> <button className="text-gray-400 text-md m-2 hover:text-gray-500">Already have an account? Click here</button></NavLink>
+          </div>
+        </form>
       </Paper>
     </div>
   )
